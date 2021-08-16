2b07c958173c247039b8b826dc5e93eda31c4dda
Armando Becerra Rodriguez <socarmaroa@gmail.com>
2021-08-15 20:15:08 -0500

    Inicio de Spring boot
    Archivos para iniciar proyecto Spring boot

---
+++ src/main/java/com/quasar/comunicador/QuasarApplication.java
@@ -0,0 +1,13 @@
+package com.quasar.comunicador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuasarApplication.class, args);
+	}
+
+}
